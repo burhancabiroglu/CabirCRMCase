6bbf2c013dc0f8d9c88e3db7e59e65d6ed2fc276
burhancabiroglu <burhancabiroglu97@gmail.com>
2025-04-27 19:19:53 +0300

    Implement error logging and set up Swagger for API documentation

--- CabirCRM.Infrastructure/obj/Debug/net9.0/CabirCRM.Infrastructure.AssemblyInfo.cs
+++ CabirCRM.Infrastructure/obj/Debug/net9.0/CabirCRM.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CabirCRM.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7419d8a9086400147998c404654674579199832c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f60903aecc726af91ee2b9e19e5d7f5c6b40ed65")]
 [assembly: System.Reflection.AssemblyProductAttribute("CabirCRM.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CabirCRM.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
